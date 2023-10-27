50aedfba62e13b87d7657d123cc990ab22d88710
anggr <anggorookky@gmail.com>
2023-10-27 10:12:06 +0800

    chore(main.ts): update description in DocumentBuilder

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
   .setTitle('Curagan API')
-  .setDescription('Curagan API')
+  .setDescription('Curagan API (okky)')
   .addBearerAuth()
   .build();
 
